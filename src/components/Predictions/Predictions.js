344d9707df59c7f28b156a103ea8d34614f47442
Alessa Messineo <apmessineo@gmail.com>
2017-07-12 17:16:09 -0400

    add sigils, handleSubmit predictions

--- src/components/Predictions/Predictions.js
+++ src/components/Predictions/Predictions.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import update from 'react-addons-update';
 import Axios from 'axios';
 
-import Banner from '../../styles/images/throne.jpg';
+import Sigils from '../../styles/images/sigils.jpg';
 
 export default class Predictions extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       user_id: "",
       user_email: "",
-      user_name: ""
+      user_name: "",
+      predictions: {},
+      error: ""
     }
   }
 
@@ -23,13 +26,62 @@ export default class Predictions extends Component {
   }
 
   showID(){
-    console.log('user id: ', this.state.user_id)
+    console.log('user id: ', this.state.user_id);
+    console.log('predictions: ', this.state.predictions.pred1);
   }
+
+  submitPredictions(){
+
+  }
+
+  handleChange(event){
+    let newState = update(this.state, {
+      predictions: {
+        $merge: {
+          [event.target.name]: event.target.value
+        }
+      }
+    });
+
+    this.setState(newState);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log('handlesubmit clicked');
+    if (!this.state.predictions.pred1 || !this.state.predictions.pred2 || !this.state.predictions.pred3 || !this.state.predictions.pred4 || !this.state.predictions.pred5 || !this.state.predictions.pred6){
+      this.setState({error: 'Please fill out all 6 predictions'})
+    } else {
+      this.submitPredictions();
+    }
+  }
+
   render(){
     return(
-      <div>
-        <h1>Hello {this.state.user_name}</h1>
+      <div className="predictions-container">
+        <h1>Hello, {this.state.user_name}</h1>
         <button onClick={this.showID.bind(this)} value="click" />
+        <img src={Sigils} className="sigils" />
+        <p> Here are the rules: Enter up to 6 predictions for season 7 of Game of Thrones, anything that has already happened or has been heavily alluded to does not count. Also, anything that you predict that might happen in the final season (season 8) does not count. It has to happen in season 7, okay?  Good luck, there's no editing these once they are submitted!<br /><br />
+        At the end of the season I'll announce a winner with the most correct predictions through email or facebook or something </p>
+        <div className="prediction-container">
+          <p className="error">{this.state.error}</p>
+          <form className="user-form" onSubmit={this.handleSubmit.bind(this)}>
+            <label>Prediction #1: </label>
+            <input name="pred1" onChange={this.handleChange.bind(this)} /><br/><br/>
+            <label>Prediction #2: </label>
+            <input name="pred2" onChange={this.handleChange.bind(this)} /><br/><br/>
+            <label>Prediction #3: </label>
+            <input name="pred3" onChange={this.handleChange.bind(this)} /><br/><br/>
+            <label>Prediction #4: </label>
+            <input name="pred4" onChange={this.handleChange.bind(this)} /><br/><br/>
+            <label>Prediction #5: </label>
+            <input name="pred5" onChange={this.handleChange.bind(this)} /><br/><br/>
+            <label>Prediction #6: </label>
+            <input name="pred6" onChange={this.handleChange.bind(this)} /><br/><br/>
+            <button type="submit" className="submit-btn">Submit</button>
+          </form>
+        </div>
       </div>
     )
   }
